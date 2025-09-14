666527f2cc437fe05c7bcd355915dfdb5be0e20a
MarcoNatalini <mnatalini@fi.uba.ar>
2025-09-14 12:40:52 -0300

    added .env config

--- app/config/config.dev.ts
+++ app/config/config.dev.ts
@@ -13,14 +13,17 @@ const isAndroid = Platform.OS === "android"
 // Detect if running on physical device vs emulator
 const isEmulator = Constants.isDevice === false
 
+// Get IP and port from environment variables
+const API_IP = process.env.EXPO_PUBLIC_API_IP 
+const API_PORT = process.env.EXPO_PUBLIC_API_PORT 
 // For Android physical devices, use your machine's IP address
 // For Android emulator, use 10.0.2.2 (emulator's localhost mapping)
 // For iOS, use localhost
 const getApiUrl = () => {
   if (isAndroid) {
-    return isEmulator ? "http://10.0.2.2:8085" : "http://192.168.0.129:8080"
+    return isEmulator ? `http://10.0.2.2:${API_PORT}` : `http://${API_IP}:${API_PORT}`
   }
-  return "http://localhost:8085"
+  return `http://localhost:${API_PORT}`
 }
 
 export default {
