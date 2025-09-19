37c7f7a2d40f156f400cd4527b08802f843ad2f2
Valen1611 <vschneider@fi.uba.ar>
2025-09-18 22:47:03 -0300

    Cambio description a mandar al back por la activity name

--- app/services/search/SearchApi.ts
+++ app/services/search/SearchApi.ts
@@ -3,5 +3,4 @@ import { SearchRequest } from "./Search.types"
 
 export const searchService = {
     search: (data: SearchRequest) => api.apisauce.post("/activities/request", data)
-    // TODO: ver a que endpoint pegarle y si estan bien los datos
 }
